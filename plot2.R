845e30287989630028900e90e56d29bc482c8dac
vatul <vatul.parakh@axtria.com>
2014-11-24 00:05:35 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -5,7 +5,6 @@ if(!exists("NEI")){
 if(!exists("SCC")){
   SCC <- readRDS("./data/Source_Classification_Code.rds")
 }
-
 # Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
 # Use the base plotting system to make a plot answering this question.
 
