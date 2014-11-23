6a3dd5331aaaa43cbc726be739b0bd3fe2422f11
vatul <vatul.parakh@axtria.com>
2014-11-24 00:07:22 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -9,7 +9,6 @@ if(!exists("SCC")){
 if(!exists("NEISCC")){
   NEISCC <- merge(NEI, SCC, by="SCC")
 }
-
 library(ggplot2)
 
 # Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
